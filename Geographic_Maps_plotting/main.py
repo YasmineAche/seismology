8b4ad81af7574a8db141c2378ed2ff3c94855dc2
YasmineAche <achemine.yasmine@gmail.com>
2026-09-15 18:21:24 +0100

    updated the code and resulted the deprecation warnings

--- Geographic_Maps_plotting/main.py
+++ Geographic_Maps_plotting/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pygmt
 import datetime
 import geopandas as gpd
+from pygmt.params import Position
 from shapely.geometry import Polygon
 
 """
@@ -52,7 +53,7 @@ data_frame_stations_b = pd.DataFrame(data=pd.read_excel("stations_type_B.xlsx"))
 print("Reading excel files and creation of a data frames complete")
 
 
-def create_accelerograph_map():
+def create_stations_map():
     """
     Create a map of North Algeria with various elements and annotations.
 
@@ -69,9 +70,11 @@ def create_accelerograph_map():
     None
     """
     xmin, xmax, ymin, ymax = [-2.6, 9, 32, 38]
-
     region = [xmin, xmax, ymin, ymax]
+
     fig = pygmt.Figure()
+
+    # Plot the coastlines and other features
     fig.coast(
         region=region,
         projection="M10c",
@@ -86,22 +89,20 @@ def create_accelerograph_map():
         ],  # a:all rivers and canals, r:all permanent rivers
         lakes="skyblue",
         frame=["a2f1"],  # plot frame and title , f:frame, a:annotation, g:grid)
-        #     Td='jTL+o1.1c+w0.8c+lO,E,S,N+o-0.1c/3c', #north arrow             ################# This or
+        Td='jTL+o1.5c+w0.5c+lO,E,S,N+o-0.1c/3c', #north arrow
     )
     ######### Title #############
-    fig.basemap(frame=["+tRéseau National Algérien d'Accélérographes"])
-
-    #fig.directional_rose('jTL+o1c+w0.5c+f2+l"W,E,S,N"')  # North arrow (rose)        ################# This
+    fig.basemap(frame=["+tTitle Of The Map"])
+    print("Adding title complete")
 
     ########### Adding Wilayas ###############
     # Path to the shape file
     map_file = "Shapefiles_69_wilayas_cleaned/wilayas.shp"
 
-    # Read the shp file
+    # Read and plot the wilayas
     wilaya_df = gpd.read_file(filename=map_file)
-
-    # Plot the Wilayas
     fig.plot(data=wilaya_df, pen="IVORY3")
+    print("Plotting of wilayas complete")
 
     ############ Adding wilaya names #################
     # Extract information about Wilayas
@@ -128,50 +129,79 @@ def create_accelerograph_map():
                 font="2.5p,NewCenturySchlbk-Italic",  # ,LIGHTGOLDENROD4
                 no_clip=True,
             )
+    print("Adding wilaya names complete")
+
+    ####### plot country borders ###########
+    # dissolve wilayas to a single country polygon
+    country_border = wilaya_df.dissolve()
+
+    # Extract all exterior coordinates
+    border_coords_list = []
+    geom = country_border.geometry.iloc[0]
+    if geom.geom_type == "Polygon":
+        border_coords_list.append(list(geom.exterior.coords))
+    elif geom.geom_type == "MultiPolygon":
+        for poly in geom.geoms:
+            border_coords_list.append(list(poly.exterior.coords))
+
+    # Plot
+    for border_coords in border_coords_list:
+        fig.plot(x=[c[0] for c in border_coords],
+                 y=[c[1] for c in border_coords],
+                 pen="0.5p,black")
+    print("Adding country border complete")
 
     ########### Plot text annotations #############
-    fig.text(text="Algérie", x=3, y=33, font="12p,Bookman-Demi,black")
-    fig.text(text="Maroc", x=-2.1, y=34, angle=90, font="7p,Bookman-Demi,black")
-    fig.text(text="Tunisie", x=8.6, y=34.3, angle=-90, font="7p,Bookman-Demi,black")
+    fig.text(text="Algeria", x=3, y=33, font="12p,Bookman-Demi,black")
+    fig.text(text="Morocco", x=-2.1, y=34, angle=90, font="7p,Bookman-Demi,black")
+    fig.text(text="Tunisia", x=8.6, y=34.3, angle=-90, font="7p,Bookman-Demi,black")
     fig.text(
-        text="Mer méditerranée",
+        text="Mediterranean sea",
         x=2.3,
         y=37.4,
         angle=10,
         font="8p,ZapfChancery-MediumItalic,black",
     )
+    print("Adding other countries' names complete")
 
     ########## PLOT STATIONS #####################
-    nombre_etna2 = len(data_frame_stations_a)  # Calculate number of stations
+    number_of_stations_a = len(data_frame_stations_a)  # Calculate number of stations
     fig.plot(
         x=data_frame_stations_a.LONG,
         y=data_frame_stations_a.LAT,
         style="i0.15c",
         fill="red",
         pen="gray",
-        label=f"{nombre_etna2} ETNA-2",
+        label=f"{number_of_stations_a} Station A",
     )
 
-    nombre_etna = len(data_frame_stations_b)  # Calculate number of stations
+    number_of_stations_b = len(data_frame_stations_b)  # Calculate number of stations
     fig.plot(
         x=data_frame_stations_b.LONG,
         y=data_frame_stations_b.LAT,
         style="i0.15c",
         fill="green",
         pen="gray",
-        label=f"{nombre_etna} ETNA",
+        label=f"{number_of_stations_b} Station B",
     )
-
+    print("Plotting stations complete")
     ########### legend and Scale bar #############
     # adjust font size only for legend and scale bar
     with pygmt.config(FONT_ANNOT_PRIMARY="6p"):
-        fig.legend(position="g-2.59/32.025+w1.8/1.3", box="+gwhite+p1p+i", S=1.3)
+        fig.legend(position="g-2.59/36.78+w1.8/1.3", box="+gwhite+p1p+i", scale=1.3)
 
     # scale bar
     # f: fancy scale (without +f a simple one line scale)
     # +u:add distance unit +l:add scale title (+ab to locate it b bottom, r right...)
-    with pygmt.config(FONT_ANNOT_PRIMARY="4p"):
-        fig.basemap(map_scale="jBR+o9.2c/0.3c+w50k+f+u")  # scale bar
+    with pygmt.config(FONT_ANNOT_PRIMARY="4p", MAP_SCALE_HEIGHT="2p"):
+        #fig.basemap(map_scale="jBR+o9.2c/0.3c+w50k+f+u")  # scale bar
+        fig.scalebar(
+            position=Position((-1.95, 37.2), cstype="mapcoords"),
+            length="50k",
+            unit=True,
+            fancy=True,
+        )
+    print("Adding legend and scale bar complete")
 
     ########### Version ################
     date = datetime.datetime.now()
@@ -182,17 +212,19 @@ def create_accelerograph_map():
         angle=0,
         font="3p,Bookman-Demi,black",
     )
+    print("Adding version complete")
 
     ########### logo #############
     fig.image(
-        imagefile="CGS_logo.png",
+        imagefile="logo.png",
         position="jTR+o0.1c/0.2c+w1c",
         box=False,
     )
+    print("Adding logo complete")
 
     ########### Inset #############
     # Create an inset
-    with fig.inset(position="g-1.32/32.025+w0.5c+o0.1c", clearance=0, box="+p1p,gold"):
+    with fig.inset(position="g-1.32/36.78+w0.5c+o0.1c", clearance=0, box="+p1p,gold"):
         # Create a figure in the inset using coast.
         fig.coast(
             region="g",
@@ -201,10 +233,12 @@ def create_accelerograph_map():
             water="white",
             dcw="DZ+gred3",
         )
+    print("Adding insert complete")
 
+    ########### Show and save map #############
+    fig.savefig("my map.png", dpi=600)
     fig.show()
-
-    fig.savefig("Réseau National d'accélérographe.png", dpi=600)
+    print("Map created and saved successfully")
 
 if __name__ == "__main__":
-    create_accelerograph_map()
+    create_stations_map()
